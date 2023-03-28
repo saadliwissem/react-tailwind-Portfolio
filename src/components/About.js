142f3c91c480e5b09141a5e7a9533dde4d9e3e84
wissem <saadliwissem88@gmail.com>
2023-03-29 00:48:27 +0100

    done with about

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 //countup
-import CountUp  from "react-countup";
+import CountUp from "react-countup";
 //intersection oberver hook
 import { useInView } from "react-intersection-observer";
 //motion
@@ -15,51 +15,73 @@ const About = () => {
   return (
     <section className="section" id="about" ref={ref}>
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg: gap-x-20 lg:gap-y-0 h-screen">
           {/*img */}
-          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
           {/*text */}
-          <div>
-            <h2>About me.</h2>
-            <h3>I am sabri l bhim yarh yaaa 7atta nta</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+          <motion.div
+           variants={fadeIn("left", 0.5)}
+           initial="hidden"
+           whileInView={"show"}
+           viewport={{ once: false, amount: 0.3 }}
+          className="flex-1">
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="h3 mb-4">I am sabri l bhim yarh yaaa 7atta nta</h3>
+            <p className="mb-6">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s,
+            </p>
             {/*state */}
-            <div className="flex">
+            <div className="flex grap-x-6 lg:grap-x-10 mb-12">
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {
-                    InView?
-                    <CountUp start={0} end={13} dura tion={3}/>: null
-                  }
+                  {InView ? <CountUp start={0} end={7} dura tion={3} /> : null}
                 </div>
                 <div className="font-primary text-sm tracking-[2px] ">
-                  year of <br/>experience
+                  year of <br />
+                  experience
                 </div>
               </div>
+              <pre> </pre>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {
-                    InView?
-                    <CountUp start={0} end={13} dura tion={3}/>: null
-                  }
+                  {InView ? <CountUp start={0} end={15} dura tion={3} /> : null}
+                  K+
                 </div>
+
                 <div className="font-primary text-sm tracking-[2px] ">
-                  year of <br/>experience
+                  projects <br />
+                  completed
                 </div>
               </div>
+              <pre> </pre>
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {
-                    InView?
-                    <CountUp start={0} end={13} dura tion={3}/>: null
-                  }
+                  {InView ? <CountUp start={0} end={13} dura tion={3} /> : null}
+                  K+
                 </div>
                 <div className="font-primary text-sm tracking-[2px] ">
-                  year of <br/>experience
+                  satisfied <br />
+                  clients
                 </div>
               </div>
             </div>
-          </div>
+            <div className="flex items-center">
+              <button className="btn btn-lg">Contact me</button>
+              <pre> </pre>
+              <a href="" className="text-gradient btn-link ">
+                {" "}
+                my portfolio
+              </a>
+            </div>
+          </motion.div>
         </div>
       </div>
     </section>
